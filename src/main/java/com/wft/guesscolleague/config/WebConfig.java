e76478a7902fa1819ea884334eab9c1d1d15b689
1WFT1 <125677570+1WFT1@users.noreply.github.com>
2026-04-24 18:48:37 +0200

    Update WebConfig.java

--- src/main/java/com/wft/guesscolleague/config/WebConfig.java
+++ src/main/java/com/wft/guesscolleague/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins(
                     "http://localhost:5173",
                     "https://guess-the-colleague-frontend.vercel.app"
